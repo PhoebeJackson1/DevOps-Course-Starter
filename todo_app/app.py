abbaebc2c0c1ba098fa713668a508f08316a5cb7
PhoebeJackson1 <phoebe.jackson@softwire.com>
2024-01-22 15:07:14 +0000

    Create new to-do items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from todo_app.data.session_items import get_items
+from flask import Flask, render_template, redirect, request
+from todo_app.data.session_items import get_items, add_item
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -10,3 +10,9 @@ app.config.from_object(Config())
 def index():
     todo_items = get_items()
     return render_template('index.html', todo_items=todo_items)
+
+@app.route('/add-item', methods=['POST'])
+def add_ite():
+    title = request.form.get('todo_title')
+    add_item(title)
+    return redirect("/")
