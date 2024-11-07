46cd939f6ccdfbfc13f1009f7e752b5f059a0fee
PhoebeJackson1 <phoebe.jackson@softwire.com>
2024-11-07 16:28:52 +0000

    use mongo db instead of trello

--- todo_app/data/item.py
+++ todo_app/data/item.py
@@ -1,4 +1,4 @@
-from todo_app.data.status import Status, get_status
+from todo_app.data.status import Status, get_status_from_string, get_status_from_trello_list_id
 
 class Item:
     def __init__(self, id, name, status: Status = Status.TO_DO):
@@ -8,4 +8,8 @@ class Item:
 
     @classmethod
     def from_trello_card(cls, card):
-        return cls(card['id'], card['name'], get_status(card['idList'])) 
+        return cls(card['id'], card['name'], get_status_from_trello_list_id(card['idList'])) 
+
+    @classmethod
+    def from_database_item(cls, item):
+        return cls(item["_id"], item["name"], get_status_from_string(item["status"]))
--- todo_app/data/status.py
+++ todo_app/data/status.py
@@ -6,7 +6,7 @@ class Status(Enum):
     DOING = "Doing"
     DONE = "Done"
 
-def get_status(list_id):
+def get_status_from_trello_list_id(list_id):
     list_id_status = {
         os.getenv("TRELLO_DONE_LIST_ID"): Status.DONE,
         os.getenv("TRELLO_DOING_LIST_ID"): Status.DOING,
@@ -14,10 +14,26 @@ def get_status(list_id):
     }
     return list_id_status[list_id]
 
-def get_list_id(status: Status):
+def get_trello_list_id_from_status(status: Status):
     status_list_id = {
         Status.DONE: os.getenv("TRELLO_DONE_LIST_ID"),
         Status.DOING: os.getenv("TRELLO_DOING_LIST_ID"),
         Status.TO_DO: os.getenv("TRELLO_TO_DO_LIST_ID"),
+    }
+    return status_list_id[status]
+
+def get_status_from_string(status_string):
+    string_to_status = {
+        'To do': Status.TO_DO,
+        'Doing': Status.DOING,
+        'Done': Status.DONE,
+    }
+    return string_to_status[status_string]
+
+def get_string_from_status(status: Status):
+    status_list_id = {
+        Status.DONE: 'Done',
+        Status.DOING: 'Doing',
+        Status.TO_DO: 'To do',
     }
     return status_list_id[status]
